51ed4195d9a110284a50127e72830c2df6eabd95
Eduardo <127260662+duardo3120@users.noreply.github.com>
2023-07-23 18:57:34 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,17 @@
-function calcularGorjeta(){
-    let valorTotal = prompt("Digite o valor da conta tota: ")
-    let gorjeta = 0.1
-    console.log(valorTotal * gorjeta);
+function calcularGorjeta() {
+    const valorTotalElement = document.getElementById("valorTotal");
+    const gorjetaElement = document.getElementById("porcentagemGorjeta");
+    const resultadoElement = document.getElementById("resultadoGorjeta");
+
+    const valorTotal = parseFloat(valorTotalElement.value);
+    const gorjeta = parseFloat(gorjetaElement.value) / 100;
+
+    if (isNaN(valorTotal) || valorTotal <= 0) {
+        resultadoElement.textContent = "Valor total inválido. Digite um valor numérico maior que zero.";
+    } else if (isNaN(gorjeta) || ![0.1, 0.2, 0.3].includes(gorjeta)) {
+        resultadoElement.textContent = "Porcentagem de gorjeta inválida. Escolha entre 10%, 20% ou 30%.";
+    } else {
+        const valorGorjeta = valorTotal * gorjeta;
+        resultadoElement.textContent = "Valor da gorjeta: " + valorGorjeta.toFixed(2);
+    }
 }
